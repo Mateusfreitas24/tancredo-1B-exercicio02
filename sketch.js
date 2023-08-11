29303560912e21c57825a9040a8f15f103b0d008
mateus24 <141456405+Mateusfreitas24@users.noreply.github.com>
2023-08-11 18:00:42 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+let cor;
+let posicaoHorizontal;
+let posicaoVertical;
+let tamanho;
+
+
+function setup() {
+  createCanvas(400, 400);
+  background("black");
+  cor =color(random(0,255),random(0,255),
+  random(0,255))
+  posicaoHorizontal = 200;
+  posicaoVertical = 200;
+  tamanho=25
+}
+
+function draw(){
+  fill(cor)
+  
+  circle(posicaoHorizontal,posicaoVertical,tamanho);
+  
+  if(mouseX < posicaoHorizontal) {
+    posicaoHorizontal -= 5
+  }
+    
+  if(mouseX> posicaoHorizontal) {
+    posicaoHorizontal += 5
+  }
+  if(mouseY > posicaoVertical) {
+    posicaoVertical += 5
+  }
+    if(mouseY < posicaoVertical) {
+    posicaoVertical -= 5
+  }
+  if(mouseIsPressed) {
+    cor= color (random(0,255), random(0,255), random(0,255), random(90,100))
+  }
+  
+  
+  
+  
+}
